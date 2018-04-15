1e052e0d198f5298cce50ae946eaa6606afdafca
Parveen Gupta <parveen.gupta@outlook.com>
2018-04-15 01:27:49 -0600

    Initial Commit

--- src/main/java/com/oskap/eas/restController/PatientController.java
+++ src/main/java/com/oskap/eas/restController/PatientController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ZNE45571 on 4/14/18.
